0d59487313de2693950e6de847d580ecc055b381
Pedro <iskol.gameplay@gmail.com>
2022-02-16 15:53:12 -0300

    added search

--- contatos/views.py
+++ contatos/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
 from django.http import Http404
-
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 def index(request):
     contatos = Contato.objects.order_by('-id').filter(
@@ -24,4 +25,22 @@ def detalhes(request, contato_id):
 
     return render(request, 'contatos/detalhes.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+    if termo is None or not termo:
+        raise Http404()
+    campos = Concat('nome', Value(' '), 'sobrenome')
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+    paginator = Paginator(contatos, 10)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
