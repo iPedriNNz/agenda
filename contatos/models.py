bc225ea7733bc4b9b45a29963b6a1143e6c06b49
Pedro <iskol.gameplay@gmail.com>
2022-02-16 13:07:41 -0300

    Initial commit

---
+++ contatos/models.py
@@ -0,0 +1,36 @@
+"""
+CONTATOS
+nome: STR * (obrigatório)
+sobrenome: STR (opcional)
+telefone: STR * (obrigatório)
+email: STR (opcional)
+data_criacao: DATETIME (automático)
+descricao: texto
+categoria: CATEGORIA (outro model)
+
+CATEGORIA
+nome: STR * (obrigatório)
+"""
+
+from django.db import models
+from django.utils import timezone
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
