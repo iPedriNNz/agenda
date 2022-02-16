8f35b59009658d39751ed7cb96f6ef66138b670e
Pedro <iskol.gameplay@gmail.com>
2022-02-16 14:26:57 -0300

    adding conditionals in adm panel

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,11 +4,11 @@ from . models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email',
-                    'data_criacao', 'categoria')
+                    'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome')
     list_per_page = 10
     search_fields = ('nome', 'sobrenome', 'telefone')
-
+    list_editable = ('telefone', 'mostrar')
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
