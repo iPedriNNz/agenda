0a85813e0bbd2d309c52ba6f574921509ecac022
Pedro <iskol.gameplay@gmail.com>
2022-02-16 15:19:28 -0300

    sorting contacts

--- contatos/views.py
+++ contatos/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.http import Http404
 
 
 def index(request):
-    contatos = Contato.objects.all()
-    paginator = Paginator(contatos, 2)
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 10)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
@@ -15,6 +18,10 @@ def index(request):
 
 def detalhes(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/detalhes.html', {
         'contato': contato
     })
