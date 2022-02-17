c17145b84b47c2cb32fdbd6c39a8253465160b5a
Pedro <iskol.gameplay@gmail.com>
2022-02-17 11:27:49 -0300

    messages config

--- accounts/views.py
+++ accounts/views.py
@@ -13,16 +13,18 @@ def login(request):
     senha = request.POST.get('senha')
     user = auth.authenticate(request, username=usuario, password=senha)
     if not user:
-        messages.error(request, 'Usuário ou senha inválidos')
-        return render(request, 'accounts/login.html')
+        messages.error(request, 'Login ou senha incorretos.')
+        return redirect('index')
     else:
         auth.login(request, user)
+        messages.success(request, 'Logado com sucesso.')
         return redirect('dashboard')
 
 
 def logout(request):
     auth.logout(request)
-    return redirect('login')
+    messages.error(request, 'Deslogado com sucesso.')
+    return redirect('index')
 
 
 def register(request):
@@ -36,27 +38,27 @@ def register(request):
     senha2 = request.POST.get('senha2')
     if not nome or not sobrenome or not email or not usuario or not senha or not senha2:
         messages.error(request, 'Preencha todos os campos.')
-        return render(request, 'accounts/register.html')
+        return redirect('register')
     try:
         validate_email(email)
     except:
         messages.error(request, 'E-mail inválido.')
-        return render(request, 'accounts/register.html')
+        return redirect('register')
     if len(senha) < 6:
         messages.error(request, 'Sua senha deve ter no mínimo seis dígitos.')
-        return render(request, 'accounts/register.html')
+        return redirect('register')
     if len(usuario) < 6:
         messages.error(request, 'Seu usuário deve ter no mínimo seis caracteres.')
-        return render(request, 'accounts/register.html')
+        return redirect('register')
     if senha != senha2:
         messages.error(request, 'Suas senhas devem ser iguais.')
-        return render(request, 'accounts/register.html')
+        return redirect('register')
     if User.objects.filter(username=usuario).exists():
         messages.error(request, 'Este usuário já está cadastrado.')
-        return render(request, 'accounts/register.html')
+        return redirect('register')
     if User.objects.filter(email=email).exists():
         messages.error(request, 'Este e-mail já está cadastrado.')
-        return render(request, 'accounts/register.html')
+        return redirect('register')
     messages.success(request, 'Registrado com sucesso! Faça seu login.')
     user = User.objects.create_user(username=usuario, email=email, password=senha, first_name=nome, last_name=sobrenome)
     user.save()
@@ -76,6 +78,5 @@ def dashboard(request):
         form = FormContato(request.POST)
         return render(request, 'accounts/dashboard.html', {'form': form})
     form.save()
-    messages.success(request, f'Contato {request.POST.get("nome")} salvo com sucesso!')
+    messages.error(request, 'Erro ao enviar formulário.')
     return redirect('dashboard')
-
