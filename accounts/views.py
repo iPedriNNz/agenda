fc3e1b80f65fced13e5bc16d784a77f8ede74b56
Pedro <iskol.gameplay@gmail.com>
2022-02-17 02:08:51 -0300

    login, logout, dashboard and logged-in user verification

--- accounts/views.py
+++ accounts/views.py
@@ -1,64 +1,67 @@
-from time import sleep
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    user = auth.authenticate(request, username=usuario, password=senha)
+    if not user:
+        messages.error(request, 'Usuário ou senha inválidos')
+        return render(request, 'accounts/login.html')
+    else:
+        auth.login(request, user)
+        return redirect('dashboard')
 
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    return redirect('login')
 
 
 def register(request):
     if request.method != 'POST':
         return render(request, 'accounts/register.html')
-
     nome = request.POST.get('nome')
     sobrenome = request.POST.get('sobrenome')
     email = request.POST.get('email')
     usuario = request.POST.get('usuario')
     senha = request.POST.get('senha')
     senha2 = request.POST.get('senha2')
-
     if not nome or not sobrenome or not email or not usuario or not senha or not senha2:
         messages.error(request, 'Preencha todos os campos.')
         return render(request, 'accounts/register.html')
-
     try:
         validate_email(email)
     except:
         messages.error(request, 'E-mail inválido.')
         return render(request, 'accounts/register.html')
-
     if len(senha) < 6:
         messages.error(request, 'Sua senha deve ter no mínimo seis dígitos.')
         return render(request, 'accounts/register.html')
-
     if len(usuario) < 6:
         messages.error(request, 'Seu usuário deve ter no mínimo seis caracteres.')
         return render(request, 'accounts/register.html')
-
     if senha != senha2:
         messages.error(request, 'Suas senhas devem ser iguais.')
         return render(request, 'accounts/register.html')
-
     if User.objects.filter(username=usuario).exists():
         messages.error(request, 'Este usuário já está cadastrado.')
         return render(request, 'accounts/register.html')
-
     if User.objects.filter(email=email).exists():
         messages.error(request, 'Este e-mail já está cadastrado.')
         return render(request, 'accounts/register.html')
-
     messages.success(request, 'Registrado com sucesso! Faça seu login.')
     user = User.objects.create_user(username=usuario, email=email, password=senha, first_name=nome, last_name=sobrenome)
     user.save()
     return redirect('login')
 
 
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
